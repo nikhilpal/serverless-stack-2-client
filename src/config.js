263063876e14d3b8e3fea9a791d3b6f79492fdef
nikhilsingh <nikhilsingh@nbnco.com.au>
2018-05-30 12:36:40 +1000

    dev config updated

--- src/config.js
+++ src/config.js
@@ -1,23 +1,23 @@
 const dev = {
-  STRIPE_KEY: "pk_test_v1amvR35uoCNduJfkqGB8RLD",
+  STRIPE_KEY: "pk_test_5SvOKfa2OxLltmewW8DSxwhj",
   s3: {
     REGION: "us-east-1",
-    BUCKET: "notes-app-2-api-dev-attachmentsbucket-6wbhcogxihbo"
+    BUCKET: "notes-app-2-api-dev-attachmentsbucket-rkg6sy9l2isg"
   },
   apiGateway: {
     REGION: "us-east-1",
-    URL: "https://api.serverless-stack.seed-demo.club/dev"
+    URL: "https://n52pao85gg.execute-api.us-east-1.amazonaws.com/dev"
   },
   cognito: {
     REGION: "us-east-1",
-    USER_POOL_ID: "us-east-1_yKOQs2dWK",
-    APP_CLIENT_ID: "1h0r8s763vnqj4id6dvguanp8f",
-    IDENTITY_POOL_ID: "us-east-1:dedfd34a-9d7e-4bf2-a1dd-ef603bac2ecb"
+    USER_POOL_ID: "us-east-1_6dUpG6OX3",
+    APP_CLIENT_ID: "6mi969c401dh277ttl154bds6a",
+    IDENTITY_POOL_ID: "us-east-1:9b0f007c-7dd9-4fae-914e-a5f7860e5e48"
   }
 };
 
 const prod = {
-  STRIPE_KEY: "pk_test_v1amvR35uoCNduJfkqGB8RLD",
+  STRIPE_KEY: "pk_test_5SvOKfa2OxLltmewW8DSxwhj",
   s3: {
     REGION: "us-east-1",
     BUCKET: "notes-app-2-api-prod-attachmentsbucket-1h5n5ttet1hy0"
